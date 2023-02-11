750583b0c17d25c56c0e062cf19d3265459c6da5
Samyuktha Patnaik <samyukthapatnaik17@gmail.com>
2023-02-11 15:26:04 +0530

    Analysis

---
+++ Deployment/Dataset.py
@@ -0,0 +1,9 @@
+import streamlit as st
+# import pandas as pd
+# import pickle
+
+st.markdown('hello')
+# Data = pickle.load(open('Analysis.pkl', 'rb'))
+# Dataset = pd.DataFrame(Data)
+# st.title("Dataset")
+# st.table(Data)
